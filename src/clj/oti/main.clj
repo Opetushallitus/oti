4d032ccef346103a08e257e56ecb2212049c542f
Markus Penttila <markus.penttila@siili.com>
2016-09-21 14:55:27 +0300

    Make production config file location configurable with -Dconfig= jvm argument

--- src/clj/oti/main.clj
+++ src/clj/oti/main.clj
@@ -4,10 +4,13 @@
               [duct.util.runtime :refer [add-shutdown-hook]]
               [duct.util.system :refer [load-system]]
               [environ.core :refer [env]]
-              [clojure.java.io :as io]))
+              [clojure.java.io :as io]
+              [taoensso.timbre :refer [info error]]))
 
 (defn -main [& args]
-  (let [system (load-system [(io/resource "oti/system.edn") (io/file "oph-configuration/config.edn")])]
-    (println "Starting HTTP server on port" (-> system :http :port))
+  (let [prod-config-path (or (:config env) "./oph-configuration/config.edn")
+        _ (info "Loading configuration from" prod-config-path)
+        system (load-system [(io/resource "oti/system.edn") (io/file prod-config-path)])]
+    (info "Starting HTTP server on port" (-> system :http :port))
     (add-shutdown-hook ::stop-system #(component/stop system))
     (component/start system)))
