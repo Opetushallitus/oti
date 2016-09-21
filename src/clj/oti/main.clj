de2e33578a86acc02f6d0ee217cfc121b92ae34b
Markus Penttila <markus.penttila@siili.com>
2016-09-21 15:22:37 +0300

    Make HTTP port configurable with environment variable oti-http-port

--- src/clj/oti/main.clj
+++ src/clj/oti/main.clj
@@ -8,9 +8,10 @@
               [taoensso.timbre :refer [info error]]))
 
 (defn -main [& args]
-  (let [prod-config-path (or (:config env) "./oph-configuration/config.edn")
+  (let [bindings {'http-port (Integer/parseInt (:oti-http-port env "3000"))}
+        prod-config-path (or (:config env) "./oph-configuration/config.edn")
         _ (info "Loading configuration from" prod-config-path)
-        system (load-system [(io/resource "oti/system.edn") (io/file prod-config-path)])]
+        system (load-system [(io/resource "oti/system.edn") (io/file prod-config-path)] bindings)]
     (info "Starting HTTP server on port" (-> system :http :port))
     (add-shutdown-hook ::stop-system #(component/stop system))
     (component/start system)))
