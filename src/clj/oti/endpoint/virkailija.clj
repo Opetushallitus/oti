39d8bed0da18d30be0400625f556ed0dbc1459c3
Markus Penttila <markus.penttila@siili.com>
2016-09-23 12:29:12 +0300

    Add wrap-json-body middleware and an endpoint for adding exam-sessions

--- src/clj/oti/endpoint/virkailija.clj
+++ src/clj/oti/endpoint/virkailija.clj
@@ -1,11 +1,17 @@
 (ns oti.endpoint.virkailija
   (:require [compojure.core :refer :all]
             [ring.util.response :refer [resource-response content-type redirect]]
-            [oti.util.auth :as auth]))
+            [oti.util.auth :as auth]
+            [clojure.spec :as s]))
 
 (defn virkailija-endpoint [{:keys [db]}]
   (-> (context "/oti/api/virkailija" []
         (GET "/user-info" {session :session}
           {:status 200
-           :body (select-keys (:identity session) [:username])}))
+           :body (select-keys (:identity session) [:username])})
+        (context "/exam-sessions" []
+          (POST "/" {exam-data :body}
+            (if (s/valid? :oti.spec/exam-session exam-data)
+              {:status 200}
+              {:status 400}))))
       (wrap-routes auth/wrap-authorization)))
