ab4d51be35d59f71e7abe20093dd00096f83226e
Markus Penttila <markus.penttila@siili.com>
2016-09-27 13:33:31 +0300

    Add validation to input fields in exam session form

--- src/cljs/oti/ui/views/exam_sessions.cljs
+++ src/cljs/oti/ui/views/exam_sessions.cljs
@@ -1,16 +1,15 @@
 (ns oti.ui.views.exam-sessions
-  (:require [oti.spec]
+  (:require [oti.spec :as spec]
             [re-frame.core :as re-frame]
             [reagent.core :as r]
             [cljs-time.format :as ctf]
             [cljs-time.coerce :as ctc]
             [cljs-time.core :as time]
-            [cljs-time.local :as local]
             [clojure.string :as str]
             [cljs.spec :as s]))
 
-(defn input-element [form-data type key placeholder & [on-change-fn]]
-  [:input {:class type
+(defn input-element [form-data invalids type key placeholder & [on-change-fn]]
+  [:input {:class (when (key invalids) "invalid")
            :type type
            :value (key @form-data)
            :placeholder placeholder
@@ -21,11 +20,11 @@
                                                 (= "number" type) (js/parseInt))]
                               (swap! form-data assoc key value))))}])
 
-(defn input-row [form-data key type label & [placeholder]]
+(defn input-row [form-data invalids key type label & [placeholder]]
   [:div.row
    [:label
     [:span.label label]
-    (input-element form-data type key (or placeholder label))]])
+    (input-element form-data invalids type key (or placeholder label))]])
 
 (def date-format (ctf/formatter "d.M.yyyy"))
 
@@ -40,38 +39,49 @@
   (->> (time/to-default-time-zone date)
        (ctf/unparse date-format)))
 
+(defn invalid-keys [form-data]
+  (let [keys (->> (s/explain-data ::spec/exam-session @form-data)
+                  ::s/problems
+                  (map #(first (:path %)))
+                  (remove nil?)
+                  set)]
+    (cond-> keys
+            (::spec/session-date keys) (conj keys ::spec/session-date-str))))
+
 (defn new-exam-session-panel []
-  (let [form-data (r/atom {::oti.spec/exam-id 1})]
+  (let [form-data (r/atom {::spec/exam-id 1})]
     (fn []
-      [:div.exam-session-form
-       [:h3 "Uusi tutkintotapahtuma"]
-       [:form
-        [:div.row
-         [:span.label "Päivämäärä ja kellonaika"]
-         (input-element
-           form-data
-           "text"
-           :oti.spec/session-date-str
-           "pp.kk.vvvv"
-           (fn [e]
-             (let [value (-> e .-target .-value)]
-               (swap! form-data assoc :oti.spec/session-date-str value :oti.spec/session-date (parse-date value)))))
-         [:div.times
-          (input-element form-data "text" :oti.spec/start-time "hh.mm")
-          [:span.dash "\u2014"]
-          (input-element form-data "text" :oti.spec/end-time "hh.mm")]]
-        (input-row form-data :oti.spec/city "text" "Koepaikan kaupunki")
-        (input-row form-data :oti.spec/street-address "text" "Koepaikan katuosoite")
-        (input-row form-data :oti.spec/other-location-info "text" "Koepaikan tilatieto")
-        (input-row form-data :oti.spec/max-participants "number" "Osallistujien enimmäismäärä" "Määrä")
-        [:div.buttons
-         [:div.right
-          [:button.button-primary
-           {:disabled (not (s/valid? :oti.spec/exam-session @form-data))
-            :on-click (fn [e]
-                        (.preventDefault e)
-                        (re-frame/dispatch [:add-exam-session @form-data]))}
-           "Tallenna"]]]]])))
+      (let [invalids (invalid-keys form-data)]
+        [:div.exam-session-form
+         [:h3 "Uusi tutkintotapahtuma"]
+         [:form
+          [:div.row
+           [:span.label "Päivämäärä ja kellonaika"]
+           (input-element
+             form-data
+             invalids
+             "text"
+             ::spec/session-date-str
+             "pp.kk.vvvv"
+             (fn [e]
+               (let [value (-> e .-target .-value)]
+                 (swap! form-data assoc ::spec/session-date-str value ::spec/session-date (parse-date value)))))
+           [:div.times
+            (input-element form-data invalids "text" ::spec/start-time "hh.mm")
+            [:span.dash "\u2014"]
+            (input-element form-data invalids "text" ::spec/end-time "hh.mm")]]
+          (input-row form-data invalids ::spec/city "text" "Koepaikan kaupunki")
+          (input-row form-data invalids ::spec/street-address "text" "Koepaikan katuosoite")
+          (input-row form-data invalids ::spec/other-location-info "text" "Koepaikan tilatieto")
+          (input-row form-data invalids ::spec/max-participants "number" "Osallistujien enimmäismäärä" "Määrä")
+          [:div.buttons
+           [:div.right
+            [:button.button-primary
+             {:disabled (not (s/valid? ::spec/exam-session @form-data))
+              :on-click (fn [e]
+                          (.preventDefault e)
+                          (re-frame/dispatch [:add-exam-session @form-data]))}
+             "Tallenna"]]]]]))))
 
 (defn exam-sessions-panel []
   (re-frame/dispatch [:load-exam-sessions])
@@ -90,7 +100,7 @@
              [:td "Enimmäismäärä"]]]
            [:tbody
             (doall
-              (for [{:oti.spec/keys [id city start-time end-time session-date street-address other-location-info max-participants]} @exam-sessions]
+              (for [{::spec/keys [id city start-time end-time session-date street-address other-location-info max-participants]} @exam-sessions]
                 ^{:key id}
                 [:tr
                  [:td (str (unparse-date session-date) " " start-time " - " end-time)]
