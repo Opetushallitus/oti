5bfa8bbbb01099a57052d8508a5dc31229b7b991
Markus Penttila <markus.penttila@siili.com>
2016-09-28 11:50:39 +0300

    Try to get the bamboo java environment correct in build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+export JAVA_HOME="${bamboo_capability_system_jdk_JDK_1_8}"
+export LEIN_JAVA_CMD="${bamboo_capability_system_jdk_JDK_1_8}/bin/java"
+export JAVA_CMD="$LEIN_JAVA_CMD"
+
 ./ci/lein clean
 ./ci/lein compile
 ./ci/lein test2junit
