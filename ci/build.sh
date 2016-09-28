1801faf25ebdab8577f3501a4d17ca3500585118
Markus Penttila <markus.penttila@siili.com>
2016-09-28 12:48:01 +0300

    Run also uberjar build through the build.sh script

--- ci/build.sh
+++ ci/build.sh
@@ -1,9 +1,29 @@
 #!/bin/sh
 
 export JAVA_HOME="${bamboo_capability_system_jdk_JDK_1_8}"
-export LEIN_JAVA_CMD="${bamboo_capability_system_jdk_JDK_1_8}/bin/java"
-export JAVA_CMD="$LEIN_JAVA_CMD"
+export PATH=$JAVA_HOME/bin:$PATH
 
-./ci/lein clean
-./ci/lein compile
-./ci/lein test2junit
+test() {
+  ./ci/lein clean
+  ./ci/lein compile
+  ./ci/lein test2junit
+}
+
+uberjar() {
+  ./ci/lein clean
+  ./ci/lein uberjar
+}
+
+command="$1"
+
+case "$command" in
+    "test" )
+        test
+        ;;
+    "uberjar" )
+        uberjar
+        ;;
+    *)
+        echo "Unknown command $command"
+        ;;
+esac
