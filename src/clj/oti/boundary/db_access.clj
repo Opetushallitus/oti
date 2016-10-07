924806b2bfc457b3c9d2fe8045bf6db70fcc86e6
Markus Penttila <markus.penttila@siili.com>
2016-10-07 15:04:24 +0300

    Use require-sql instead of defqueries to clean-up the db-access ns

--- src/clj/oti/boundary/db_access.clj
+++ src/clj/oti/boundary/db_access.clj
@@ -1,5 +1,5 @@
 (ns oti.boundary.db-access
-  (:require [yesql.core :refer [defqueries]]
+  (:require [yesql.core :refer [require-sql]]
             [clojure.java.jdbc :as jdbc]
             [duct.component.hikaricp]
             [oti.spec :as spec]
@@ -8,7 +8,7 @@
   (:import [duct.component.hikaricp HikariCP]))
 
 
-(defqueries "oti/queries.sql")
+(require-sql ["oti/queries.sql" :as q])
 
 (defn- translation-by-key-fn [key]
   (fn [ts e]
@@ -73,7 +73,7 @@
                                                       city
                                                       other-location-info
                                                       lang exam-session-id)
-        db-fn               (if update? update-exam-session-translation! insert-exam-session-translation!)]
+        db-fn               (if update? q/update-exam-session-translation! q/insert-exam-session-translation!)]
     (db-fn translation {:connection tx})))
 
 (defn store-exam-session-translations [tx exam-session update?]
@@ -83,31 +83,31 @@
       (throw (Exception. "Error occured inserting translations. Missing exam session id.")))))
 
 (defn insert-exam-session [tx exam-session]
-  (or (insert-exam-session<! exam-session {:connection tx})
+  (or (q/insert-exam-session<! exam-session {:connection tx})
       (throw (Exception. "Could not create new exam session."))))
 
 (defn store-registration! [tx {::spec/keys [session-id language-code sections]} external-user-id]
   (let [conn {:connection tx}]
     (let [registarable-sections (remove (fn [[_ options]] (::spec/accredit? options)) sections)]
       (when (pos? (count registarable-sections))
-        (if-let [reg-id (:id (insert-registration<! {:session-id session-id
+        (if-let [reg-id (:id (q/insert-registration<! {:session-id session-id
                                                      :external-user-id external-user-id
                                                      :language-code (name language-code)} conn))]
           (doseq [[section-id opts] registarable-sections]
             (let [params {:section-id section-id
                           :external-user-id external-user-id
                           :registration-id reg-id}]
-              (insert-section-registration! params conn)
-              (let [all-modules (->> (select-modules-for-section params conn) (map :id) set)
+              (q/insert-section-registration! params conn)
+              (let [all-modules (->> (q/select-modules-for-section params conn) (map :id) set)
                     register-modules (or (seq (::spec/retry-modules opts))
                                          (cs/difference all-modules (::spec/accredit-modules opts)))]
                 (doseq [module-id register-modules]
-                  (insert-module-registration! (assoc params :module-id module-id) conn))
+                  (q/insert-module-registration! (assoc params :module-id module-id) conn))
                 (doseq [module-id (::spec/accredit-modules opts)]
-                  (insert-module-accreditation! (assoc params :module-id module-id) conn)))))
+                  (q/insert-module-accreditation! (assoc params :module-id module-id) conn)))))
           (throw (Exception. "No registeration id received.")))))
     (doseq [[section-id _] (filter (fn [[_ options]] (::spec/accredit? options)) sections)]
-      (insert-section-accreditation! {:section-id section-id :external-user-id external-user-id} conn))))
+      (q/insert-section-accreditation! {:section-id section-id :external-user-id external-user-id} conn))))
 
 (defprotocol DbAccess
   (upcoming-exam-sessions [db])
@@ -123,27 +123,27 @@
 (extend-type HikariCP
   DbAccess
   (upcoming-exam-sessions [{:keys [spec]}]
-    (group-exam-session-translations (exam-sessions-in-future {} {:connection spec})))
+    (group-exam-session-translations (q/exam-sessions-in-future {} {:connection spec})))
   (published-exam-sessions-with-space-left [{:keys [spec]}]
-    (group-exam-session-translations (published-exam-sessions-in-future-with-space-left {} {:connection spec})))
+    (group-exam-session-translations (q/published-exam-sessions-in-future-with-space-left {} {:connection spec})))
   (exam-session [{:keys [spec]} id]
-    (-> (exam-session-by-id {:id id} {:connection spec}) group-exam-session-translations))
+    (-> (q/exam-session-by-id {:id id} {:connection spec}) group-exam-session-translations))
   (add-exam-session! [{:keys [spec]} exam-session]
     (jdbc/with-db-transaction [tx spec]
       (let [exam-session-id (:id (insert-exam-session tx exam-session))]
         (store-exam-session-translations tx (assoc exam-session ::spec/id exam-session-id) false))))
   (save-exam-session! [{:keys [spec]} exam-session]
     (jdbc/with-db-transaction [tx spec]
-      (update-exam-session! exam-session {:connection tx})
+      (q/update-exam-session! exam-session {:connection tx})
       (store-exam-session-translations tx exam-session :update)))
   (remove-exam-session! [{:keys [spec]} id]
-    (delete-exam-session-translations! {:exam-session-id id} {:connection spec})
-    (delete-exam-session! {:exam-session-id id} {:connection spec}))
+    (q/delete-exam-session-translations! {:exam-session-id id} {:connection spec})
+    (q/delete-exam-session! {:exam-session-id id} {:connection spec}))
   (modules-available-for-user [{:keys [spec]} external-user-id]
-    (->> (select-modules-available-for-user {:external-user-id external-user-id} {:connection spec})
+    (->> (q/select-modules-available-for-user {:external-user-id external-user-id} {:connection spec})
          group-sections-and-modules))
   (valid-full-payments-for-user [{:keys [spec]} external-user-id]
-    (-> (select-valid-payment-count-for-user {:external-user-id external-user-id} {:connection spec})
+    (-> (q/select-valid-payment-count-for-user {:external-user-id external-user-id} {:connection spec})
         first
         :count))
   (register! [{:keys [spec]} registration-data external-user-id]
