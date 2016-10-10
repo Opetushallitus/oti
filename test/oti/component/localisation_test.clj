cbf4dbd67f922e6acdc3039f2825452f213e553b
Jaakko Salokanta <jaakko.salokanta@cybercom.com>
2016-10-10 14:54:32 +0300

    Initial sketch of localisation service integration

---
+++ test/oti/component/localisation_test.clj
@@ -0,0 +1,7 @@
+(ns oti.component.localisation-test
+  (:require [clojure.test :refer :all]
+            [oti.component.localisation :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, even if I don't fail."
+    (is (= 1 1))))
