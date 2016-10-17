caa89b29396379eb50ffa1d3a6e28a50ace8aa33
Markus Penttila <markus.penttila@siili.com>
2016-10-17 15:47:46 +0300

    Migration 12 needs default values for not null columns

--- resources/oti/migrations/012-evaluation-bounds-in-db.up.sql
+++ resources/oti/migrations/012-evaluation-bounds-in-db.up.sql
@@ -1,11 +1,11 @@
 ALTER TABLE section
-    ADD COLUMN is_executed_as_whole BOOLEAN NOT NULL;
+    ADD COLUMN is_executed_as_whole BOOLEAN NOT NULL DEFAULT FALSE;
 
 ALTER TABLE module
-    ADD COLUMN has_points BOOLEAN NOT NULL;
+    ADD COLUMN has_points BOOLEAN NOT NULL DEFAULT TRUE;
 
 ALTER TABLE module
-    ADD COLUMN is_accepted_separately BOOLEAN NOT NULL;
+    ADD COLUMN is_accepted_separately BOOLEAN NOT NULL DEFAULT TRUE;
 
 ALTER TABLE module_score
     ALTER points DROP NOT NULL;
