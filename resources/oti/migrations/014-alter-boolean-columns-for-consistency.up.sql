eeb4aef987330dbd6a9030bcb221ab1722f6e2f5
Jaakko Salokanta <jaakko.salokanta@cybercom.com>
2016-10-17 13:06:16 +0300

    Remove boolean inuendo from column names

---
+++ resources/oti/migrations/014-alter-boolean-columns-for-consistency.up.sql
@@ -0,0 +1,3 @@
+ALTER TABLE section RENAME COLUMN is_executed_as_whole TO executed_as_whole;
+ALTER TABLE module RENAME COLUMN has_points TO points;
+ALTER TABLE module RENAME COLUMN is_accepted_separately TO accepted_separately;
