5510729f38969dd17a37134c03b78b970ca26a93
Jaakko Salokanta <jaakko.salokanta@cybercom.com>
2016-10-18 15:57:28 +0300

    Result page back button knows the lang

--- src/cljs/oti/ui/registration/views/registration_result.cljs
+++ src/cljs/oti/ui/registration/views/registration_result.cljs
@@ -1,9 +1,12 @@
 (ns oti.ui.registration.views.registration-result
-  (:require [oti.ui.i18n :refer [t]]))
+  (:require [oti.ui.i18n :refer [t]]
+            [re-frame.core :as rf]))
 
 (defn result-panel [participant-data]
-  [:div
-   [:h3 (:registration-message participant-data)]
-   [:div.buttons
-    [:div.left
-     [:a.button {:href "/oti/abort"} (t "Palaa alkuun")]]]])
+  (let [lang (rf/subscribe [:language])]
+    (fn [participant-data]
+      [:div
+       [:h3 (:registration-message participant-data)]
+       [:div.buttons
+        [:div.left
+         [:a.button {:href (str "/oti/abort?lang=" (name @lang))} (t "back-to-start" "Palaa alkuun")]]]])))
