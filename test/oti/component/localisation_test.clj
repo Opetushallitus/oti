49ac3368ef1d9c4cc75dbe8a3b3dcc26fd64fd3a
Markus Penttila <markus.penttila@siili.com>
2016-10-24 16:19:02 +0300

    Add test for vetuma-payment component

--- test/oti/component/localisation_test.clj
+++
@@ -1,7 +0,0 @@
-(ns oti.component.localisation-test
-  (:require [clojure.test :refer :all]
-            [oti.component.localisation :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, even if I don't fail."
-    (is (= 1 1))))
