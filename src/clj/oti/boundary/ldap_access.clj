602ac9a4dff60acadbdfe212f66a2c52ad937ed3
Markus Penttila <markus.penttila@siili.com>
2016-10-25 11:01:02 +0300

    Create LDAP connection when first required, not on application start

--- src/clj/oti/boundary/ldap_access.clj
+++ src/clj/oti/boundary/ldap_access.clj
@@ -1,20 +1,4 @@
-(ns oti.boundary.ldap-access
-  (:require [clj-ldap.client :as ldap]
-            [clojure.string :as str]
-            [cheshire.core :as json]
-            [oti.component.ldap])
-  (:import [oti.component.ldap Ldap]))
-
-(def people-path-base "ou=People,dc=opintopolku,dc=fi")
-(def user-right-name "APP_OTI_CRUD")
+(ns oti.boundary.ldap-access)
 
 (defprotocol LdapAccess
   (user-has-access? [ldap username]))
-
-(extend-type Ldap
-  LdapAccess
-  (user-has-access? [{:keys [pool]} username]
-    (when-let [user (first (ldap/search pool people-path-base {:filter (str "(uid=" username ")")}))]
-      (->> (json/parse-string (:description user))
-           (filter #(str/includes? % user-right-name))
-           first))))
--- src/clj/oti/component/ldap.clj
+++ src/clj/oti/component/ldap.clj
@@ -1,9 +1,15 @@
 (ns oti.component.ldap
   (:require [com.stuartsierra.component :as component]
-            [clj-ldap.client :as ldap])
+            [clj-ldap.client :as ldap]
+            [oti.boundary.ldap-access :as ldap-access]
+            [cheshire.core :as json]
+            [clojure.string :as str])
   (:import [java.net InetAddress]))
 
-(defn create-ldap-connection-pool [ldap-config]
+(def people-path-base "ou=People,dc=opintopolku,dc=fi")
+(def user-right-name "APP_OTI_CRUD")
+
+(defn- create-ldap-connection-pool [ldap-config]
   (let [host        (InetAddress/getByName (:server ldap-config))
         host-name   (.getHostName host)]
     (ldap/connect {:host            [{:address host-name
@@ -11,16 +17,26 @@
                    :bind-dn         (:userdn ldap-config)
                    :password        (:password ldap-config)
                    :ssl?            (:ssl ldap-config)
-                   :num-connections 1})))
+                   :num-connections 1
+                   :connect-timeout 10000
+                   :timeout         15000})))
 
 (defrecord Ldap [config]
   component/Lifecycle
   (start [this]
-    (assoc this :pool (create-ldap-connection-pool config)))
-  (stop [{:keys [pool] :as this}]
-    (when pool
-      (ldap/close pool))
-    (assoc this :pool nil)))
+    (assoc this :pool (atom nil)))
+  (stop [{:keys [pool]}]
+    (when @pool
+      (ldap/close @pool))
+    (reset! pool nil))
+  ldap-access/LdapAccess
+  (user-has-access? [{:keys [pool]} username]
+    (when-not @pool
+      (reset! pool (create-ldap-connection-pool config)))
+    (when-let [user (first (ldap/search @pool people-path-base {:filter (str "(uid=" username ")")}))]
+      (->> (json/parse-string (:description user))
+           (filter #(str/includes? % user-right-name))
+           first))))
 
 (defn ldap [config]
   (->Ldap config))
