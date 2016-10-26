0826832b9f68572491c36f5497a076c5d0a79c56
Markus Penttila <markus.penttila@siili.com>
2016-10-26 14:22:47 +0300

    Poll every 10 minutes for unsent email and try to send it

--- src/clj/oti/component/email_service.clj
+++ src/clj/oti/component/email_service.clj
@@ -9,8 +9,8 @@
             [clojure.spec :as s]))
 
 (defprotocol EmailSender
-  (send-email-by-payment-order-number! [this email-params])
-  (send-queued-mails! [this]))
+  (send-email-by-payment-order-number! [this db email-params])
+  (send-queued-mails! [this db]))
 
 (defn- send-email-via-service! [{:keys [email-service-url]} {:keys [recipients subject body]}]
   {:pre [(every? #(identity %) [recipients subject body]) (s/valid? :oti.spec/email (first recipients))]}
@@ -27,11 +27,11 @@
         true)
       (log/error "Could not send email to" recipients ", HTTP status from email service was" status))))
 
-(defn- add-email-to-queue! [{:keys [db]} {:keys [order-number subject body] :as params}]
+(defn- add-email-to-queue! [db {:keys [order-number subject body] :as params}]
   {:pre [(every? #(identity %) [order-number subject body])]}
   (dba/add-email-by-payment-order-number! db params))
 
-(defn- send-emails! [{:keys [db] :as this}]
+(defn- send-emails! [this db]
   (jdbc/with-db-transaction [tx (:spec db) {:isolation :serializable}]
     (doseq [{:keys [id recipient subject body]} (dba/unsent-emails-for-update db tx)]
       (when (send-email-via-service! this {:recipients [recipient] :body body :subject subject})
@@ -42,11 +42,11 @@
   (start [this] this)
   (stop [this] this)
   EmailSender
-  (send-email-by-payment-order-number! [this email-params]
-    (add-email-to-queue! this email-params)
-    (send-emails! this))
-  (send-queued-mails! [this]
-    (send-emails! this)))
+  (send-email-by-payment-order-number! [this db email-params]
+    (add-email-to-queue! db email-params)
+    (send-emails! this db))
+  (send-queued-mails! [this db]
+    (send-emails! this db)))
 
 (defn email-service [config]
   (map->EmailService config))
