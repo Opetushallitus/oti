ffa8f06ae163d37dab47e56bd152cb02160ff5c0
Markus Penttila <markus.penttila@siili.com>
2016-11-11 15:08:56 +0200

    Set cookie max-age to 8 hours to match CAS ticket age, move configuration to config files

--- src/clj/oti/util/session.clj
+++ src/clj/oti/util/session.clj
@@ -2,6 +2,7 @@
   (:require [ring.middleware.session :as rms]
             [ring.middleware.session.cookie :as rmsc]))
 
-(defn wrap-session [handler session-key]
-  (rms/wrap-session handler {:store (rmsc/cookie-store {:key session-key})
-                             :cookie-attrs {:max-age 3600}}))
+(defn wrap-session [handler {:keys [key cookie-attrs]}]
+  (rms/wrap-session handler {:store (rmsc/cookie-store {:key key})
+                             :cookie-name "oti-session"
+                             :cookie-attrs cookie-attrs}))
