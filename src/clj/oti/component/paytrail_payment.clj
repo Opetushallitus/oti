7d5fa0e0587bd43e478d12379d8f6e89600c040c
Tommi Helander <tommi.helander@gofore.com>
2017-10-05 17:17:26 +0300

    Fix PARAMS_OUT to include proper fields

--- src/clj/oti/component/paytrail_payment.clj
+++ src/clj/oti/component/paytrail_payment.clj
@@ -26,7 +26,7 @@
   {:pre  [(s/valid? ::os/pt-payment-params params)]
    :post [(s/valid? ::os/pt-payment-form-data %)]}
   (let [params-in "MERCHANT_ID,LOCALE,URL_SUCCESS,URL_CANCEL,AMOUNT,ORDER_NUMBER,REFERENCE_NUMBER,MSG_SETTLEMENT_PAYER,MSG_UI_MERCHANT_PANEL,PARAMS_IN,PARAMS_OUT"
-        params-out "PAYMENT_ID,TIMESTAMP,STATUS,RETURN_AUTHCODE"
+        params-out "ORDER_NUMBER,PAYMENT_ID,AMOUNT,TIMESTAMP,STATUS"
         form-params #:oti.spec{:MERCHANT_ID  merchant-id
                                :LOCALE       (case language-code :fi "fi_FI" :sv "sv_SE")
                                :URL_SUCCESS  (str oti-paytrail-uri "/success")
@@ -42,7 +42,7 @@
     #:oti.spec{:uri                 paytrail-host
                :pt-payment-form-params (assoc form-params ::os/AUTHCODE authcode)}))
 
-(def response-keys [:PAYMENT_ID :TIMESTAMP :STATUS])
+(def response-keys [:ORDER_NUMBER :PAYMENT_ID :AMOUNT :TIMESTAMP :STATUS])
 
 (defn- return-authcode-valid? [{:keys [merchant-secret]} form-data]
   (if-let [return-authcode (:RETURN_AUTHCODE form-data)]
