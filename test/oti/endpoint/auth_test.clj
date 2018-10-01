9c4812f7744af4fb7220da24d7288ebeba04121e
Jarkko Pätäri <jarkko.patari@gofore.com>
2018-10-01 16:20:37 +0300

    Fix user details request
    OTI-89

--- test/oti/endpoint/auth_test.clj
+++ test/oti/endpoint/auth_test.clj
@@ -37,6 +37,7 @@
 
 (def url-helper (->> (uh/url-helper {:virkailija-host "itest-virkailija.oph.ware.fi"
                                      :oti-host "http://localhost:3000"
+                                     :alb-host "itest-virkailija.oph.ware.fi"
                                      :tunnistus-host "tunnistus-testi.opintopolku.fi"})
                      (component/start)))
 
