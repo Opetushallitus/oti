00552c02c5082faf23193fcaac31be5def1ccfb7
Jarkko Pätäri <jarkko.patari@gofore.com>
2019-08-14 12:47:01 +0300

    Try semicolon as csv separator
    OTI-138

--- src/clj/oti/util/csv.clj
+++ src/clj/oti/util/csv.clj
@@ -6,6 +6,6 @@
 (defn csv-output [data]
   (let [headers (map name (keys (first data)))
         rows (map vals data)
-        stream-csv (fn [ostream] (csv/write-csv ostream (cons headers rows) :separator \tab)
+        stream-csv (fn [ostream] (csv/write-csv ostream (cons headers rows) :separator \;)
                                  (.flush ostream))]
     (ring-io/piped-input-stream #(stream-csv (io/make-writer % {})))))
